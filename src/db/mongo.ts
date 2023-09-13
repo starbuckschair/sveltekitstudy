bd3a8bf84e7ae2df9bc8d586ebcc01701586c8d3
starbuckschair <93638998+starbuckschair@users.noreply.github.com>
2023-09-13 13:19:03 +0900

    환경변수 변경

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -2,7 +2,8 @@
 
 import { MongoClient } from 'mongodb';
 
-import { MONGO_URL } from '$env/static/private';
+// import { MONGO_URL } from '$env/static/private';
+import { MONGO_URL } from '$lib/Env';
 
 console.log('url >>> ' + MONGO_URL);
 
---
+++ src/lib/Env.js
@@ -0,0 +1,1 @@
+export const MONGO_URL = import.meta.env.VITE_MONGO_URL;
